290112943b399a03f59f8ab64f899d1baebdcecb
unknown <JeevanReddy.Yemireddy@lntinfotech.com>
2020-09-09 17:39:18 +0530

    Given File

---
+++ twonumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace twonumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("enter two numbers");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int product;
+            if (a == 0)
+            {
+                Console.WriteLine("Enter positive number");
+                a = Convert.ToInt32(Console.ReadLine());
+            }
+            sum = a + b;
+            product = a * b;
+            Console.WriteLine(sum);
+            Console.WriteLine(product);
+            Console.ReadKey();
+        }
+    }
+}
